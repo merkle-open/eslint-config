ecb28031be362b663725d78cca0903ae625cb0c9
Simon Mollweide <simon.mollweide@namics.com>
2017-03-02 19:24:45 +0100

    [doc] update documentation for require-jsdoc

--- test/es5/rules/style/require-jsdoc.js
+++ test/es5/rules/style/require-jsdoc.js
@@ -2,7 +2,22 @@
 // DESCRIPTION = do not require jsdoc
 // STATUS = 2
 
-// <!START
+/* eslint no-unused-vars: 0*/
 
+// <!START
+// BAD
+/*
+function test() {
+	return 'test';
+}
+*/
+
+// GOOD
+/**
+ * @returns {string} - something
+ */
+function test() {
+	return 'test';
+}
 // END!>
 document.window.append('', null);
